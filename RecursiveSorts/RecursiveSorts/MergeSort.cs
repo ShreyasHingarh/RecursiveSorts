4875a6316c7cec8d43ffccb2889d81efa7c75ef1
GMR Student <gmrstudent@outlook.com>
2022-04-10 11:25:29 -0700

    yes

---
+++ RecursiveSorts/RecursiveSorts/MergeSort.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursiveSorts
+{
+    class MergeSort<T> where T : IComparable<T>
+    {
+        public void Sort(T[] array)
+        {
+            if (array.Length < 2)
+            {
+                return;
+            }
+            (T[] left, T[] right) = Split(array);
+            Sort(left);
+            Sort(right);
+            Merge(array, left, right);
+        }
+
+        public (T[] left, T[] right) Split(T[] array)
+        {
+
+            T[] left = new T[array.Length / 2];
+            T[] right = new T[array.Length - left.Length];
+            for (int i = 0; i < left.Length; i++)
+            {
+                left[i] = array[i];
+            }
+            for (int i = 0; i < right.Length; i++)
+            {
+                right[i] = array[i + left.Length];
+            }
+            return (left, right);
+        }
+
+        public void Merge(T[] array, T[] left, T[] right)
+        {
+            int rightIndex = 0;
+            int leftIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(leftIndex >= left.Length)
+                {
+                    Array.Copy(right, rightIndex, array, i, right.Length - rightIndex);
+                    return;
+                }
+                if(rightIndex >= right.Length)
+                {
+                    Array.Copy(left, leftIndex, array, i, left.Length - leftIndex);
+                    return;
+                }
+
+                if (left[leftIndex].CompareTo(right[rightIndex]) < 0)
+                {
+                    array[i] = left[leftIndex];
+                    leftIndex++;
+                }
+                else if (left[leftIndex].CompareTo(right[rightIndex]) >= 0)
+                {
+                    array[i] = right[rightIndex];
+                    rightIndex++;
+                }
+            }
+
+        }
+    }
+}
--- RecursiveSorts/RecursiveSorts/Program.cs
+++ RecursiveSorts/RecursiveSorts/Program.cs
@@ -6,7 +6,13 @@ namespace RecursiveSorts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MergeSort<int> sort = new MergeSort<int>();
+            int[] nums = new int[]{ 10, 19, 8, 7, 6, 4, 9, 13, 16, 1 };
+            sort.Sort(nums);
+            for(int i = 0;i < nums.Length;i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
         }
     }
 }
