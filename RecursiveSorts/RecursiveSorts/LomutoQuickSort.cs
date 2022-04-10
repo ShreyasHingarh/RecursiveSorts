d2cf0fe466147e09a4c2df15f66f392400129fb7
GMR Student <gmrstudent@outlook.com>
2022-04-10 11:31:25 -0700

    yes

---
+++ RecursiveSorts/RecursiveSorts/LomutoQuickSort.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursiveSorts
+{
+    class LomutoQuickSort <T> where T : IComparable<T>
+    {
+        public int wallIndex = 0;
+
+    }
+}
